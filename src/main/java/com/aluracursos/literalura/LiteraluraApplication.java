c39ec42eaf4a6f77c1646ba5dfe8b926850e11c7
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-02-16 20:29:51 -0600

    Configuración inicial de Spring Boot 3.2.3 y DB exitosa

--- src/main/java/com/aluracursos/literalura/LiteraluraApplication.java
+++ src/main/java/com/aluracursos/literalura/LiteraluraApplication.java
@@ -1,13 +1,20 @@
 package com.aluracursos.literalura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.Principal;
+
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+    public void run(String... args) throws Exception {
+//        Principal principal = new Principal(repository);
+//        principal.muestraElMenu();
+    }
 }
