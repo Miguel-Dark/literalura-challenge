7aae8741a0b213fe0b82563f6ccdb241b37cfa0e
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-03-07 21:41:06 -0600

    feat: create AutorRepository and LibroRepository with derived queries for persistence

--- src/main/java/com/aluracursos/literalura/model/Libro.java
+++ src/main/java/com/aluracursos/literalura/model/Libro.java
@@ -27,12 +27,45 @@ public class Libro {
         this.numeroDeDescargas = datosLibro.numeroDeDescargas();
     }
 
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public Double getNumeroDeDescargas() {
+        return numeroDeDescargas;
+    }
+
+    public void setNumeroDeDescargas(Double numeroDeDescargas) {
+        this.numeroDeDescargas = numeroDeDescargas;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
     }
 
     @Override
---
+++ src/main/java/com/aluracursos/literalura/repository/AutorRepository.java
@@ -0,0 +1,12 @@
+package com.aluracursos.literalura.repository;
+
+import com.aluracursos.literalura.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+    List<Autor> findByFechaDeNacimientoLessThanEqualAndFechaDeFallecimientoGreaterThanEqual(
+            Integer fechaNacimiento,
+            Integer fechaFallecimiento);
+}
